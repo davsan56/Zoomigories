b15c3cc7e23087a7e6aa14b49775666e4c461f98
David San Antonio <davsan56@gmail.com>
2021-01-15 20:47:43 -0500

    Initial Commit

---
+++ Scatagories/ScatagoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScatagoriesApp.swift
+//  Scatagories
+//
+//  Created by David San Antonio on 1/15/21.
+//
+
+import SwiftUI
+
+@main
+struct ScatagoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
