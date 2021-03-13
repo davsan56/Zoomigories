0d61bd6bd116e435a6c41c3705d3fd5f4704fca4
David San Antonio <davsan56@gmail.com>
2021-03-12 21:10:00 -0500

    Fixing table and some buttons on scoreboardview

--- Zoomigories/view/game/timer/TimerManager.swift
+++ Zoomigories/view/game/timer/TimerManager.swift
@@ -8,19 +8,23 @@
 import Foundation
 
 class TimerManager: ObservableObject {
-    var timer: Timer? = nil
+    var timer: Timer = Timer()
     @Published var timeRemaining = Env.isProduction() ? 180 : 10
     @Published var timerDone = false
     
+    init() {
+        timer = Timer(timeInterval: 1.0, target: self, selector: #selector(subtractTimer), userInfo: nil, repeats: true)
+    }
+    
     func startTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(subtractTimer), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer, forMode: .common)
     }
     
     @objc func subtractTimer() {
         timeRemaining -= 1
         if timeRemaining == 0 {
             timerDone = true
-            timer?.invalidate()
+            timer.invalidate()
         }
     }
 }
